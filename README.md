327c39533d86d82b3bb420ed73c5afa0936f8d53
Michael55555 <michaelpeters5647@gmail.com>
2020-07-20 15:21:55 +0200

    Add chart with sequence length per number

---
+++ README.md
@@ -0,0 +1,2 @@
+# Collatz Problem Sequence Chart
+Make sure you check out the [website](https://collatz-problem.vercel.app/)
--- script.js
+++ script.js
@@ -1,25 +1,51 @@
-function setup() {
-    console.log('Setting up p5.js ...');
-    createCanvas(innerWidth, innerHeight);
+let chart;
+
+/**
+ * The amount of steps it takes to reach 1.
+ * @param {number} num The number to check
+ */
+function collatzProblemSteps(num) {
+    let steps = 0;
+    if (num === 0) throw new Error('One is not a valid number for this function')
+    while (num !== 1) {
+        console.log((num !== 1))
+        if ((num % 2) === 0) {
+            num /= 2;
+        } else {
+            num = (num * 3) + 1
+        }
+        steps++;
+    }
+    return steps;
 }
 
-function draw() {
-    background('#fff');
-    translate(width / 2, height / 2);
-    stroke('#0f0f0f');
-    strokeWeight(1.5)
-    const arr = [];
-    for (let x = -(innerWidth / 2); x < (innerWidth / 2); x += 1) {
-        arr[arr.length] = [x, f(x)];
+onload = () => {
+    const ctx = document.querySelector('canvas');
+    ctx.style.height = innerHeight;
+    ctx.style.width = innerWidth;
+    const data = [];
+    const labels = [];
+    for (let i = 0; i < 1000; i++) {
+        labels[i] = `${i + 1}`
+        data[i] = collatzProblemSteps(i + 1)
     }
 
-    for (let x = 1; x < arr.length; x+=1) {
-        line(...arr[x-1], ...arr[x])
-    }
-
-    line(...arr);
-}
-
-function f(x) {
-    return Math.pow(x * 1.01, 3) - 12 * Math.pow(x, 2) + 9 * x + 2;
-}
+    chart = new Chart(ctx, {
+        type: 'line',
+        options: {
+            elements: {
+                point: {
+                    radius: 0,
+                }
+            }
+        },
+        data: {
+            labels,
+            datasets: [{
+                borderWidth: 0,
+                label: 'Steps',
+                data
+            }]
+        }
+    });
+};
